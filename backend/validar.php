4c34025f433270900141fdd248f1cc5fecaea383
agustin <agustinquetto@gmail.com>
2018-04-27 12:01:19 -0300

    2704-3

--- backend/validar.php
+++ backend/validar.php
@@ -4,23 +4,18 @@ $user = isset($_POST["user"]) ? $_POST["user"] : NULL;
 $password = isset($_POST["password"]) ? $_POST["password"] : NULL;
 
 $filename = 'users.txt';
-$fp = @fopen($filename, 'r'); 
+$fp = fopen($filename, 'r');
 if ($fp) {
    $array = explode("\n", fread($fp, filesize($filename)));
 }
 
-echo var_dump($array);
-
-foreach ($array as $value) {
-    $user[0] = explode('-', $value);
-}
-
-echo var_dump($user[0]);
-
-if($user == 'Agustin' && $password == '123456' || $user == 'Pancho' && $password == '123456' || $user == 'Tototo' && $password == '123456') {
-    echo 'ok';
-} else {
-    echo 'failed';
+for($i = 0; $i < count($array); $i++) {
+    $users[$i] = explode('-', $array[$i]);
+    
+    if($user == trim($users[0][0]) && $password == trim($users[0][1])) {
+        echo 'ok';
+        break;
+    }
 }
 
 ?>
--- frontend/ts/main.ts
+++ frontend/ts/main.ts
@@ -26,7 +26,46 @@ function validar():void{
             console.log(response)
 
             if (response === 'ok') {
-                (<HTMLDivElement> document.getElementById("message")).innerHTML = '<img src="./drib_nudes_1x.jpg"/>';
+                //(<HTMLDivElement> document.getElementById("message")).innerHTML = '<img src="./drib_nudes_1x.jpg"/>';
+                document.body.style.backgroundColor = 'green';
+            } else {
+                document.body.style.backgroundColor = 'red';
+            }
+        }
+    }
+    
+    xmlhttp.open("POST", "../backend/validar.php", true);
+    xmlhttp.setRequestHeader("content-type","application/x-www-form-urlencoded");
+    xmlhttp.send("user="+user+"&password="+password);
+
+}
+
+function mostrar():void{
+
+    let user : string = (<HTMLInputElement> document.getElementById('account')).value
+    let password : string = (<HTMLInputElement> document.getElementById('password')).value
+    
+    if (user.length < 6) { 
+        (<HTMLDivElement> document.getElementById("message")).innerHTML = "El usuario posee menos de 6 caracteres";
+        console.log(user.length)
+        return;
+    }
+
+    if (password.length < 6) { 
+        (<HTMLDivElement> document.getElementById("message")).innerHTML = "La contraseña posee menos de 6 caracteres";
+        return;
+    }
+    
+    let xmlhttp : XMLHttpRequest = new XMLHttpRequest();
+    
+    xmlhttp.onreadystatechange = () => {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            let response : string = xmlhttp.responseText;
+            response = response === "" ? "failed" : response;
+            console.log(response)
+
+            if (response === 'ok') {
+                //(<HTMLDivElement> document.getElementById("message")).innerHTML = '<img src="./drib_nudes_1x.jpg"/>';
                 document.body.style.backgroundColor = 'green';
             } else {
                 document.body.style.backgroundColor = 'red';
